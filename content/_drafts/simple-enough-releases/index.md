8b8b2e0f1350da35cc2c4e698861b7309ef9e52c
Chance <chancesmithb@gmail.com>
2021-04-02 15:03:03 -0500

    📝  CONTENT: [glossary] add 3Ws

---
+++ content/_drafts/simple-enough-releases/index.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title: "Essential Releases: Smiple Enough to Ship"
+date: "2021-04-07T08Z"
+tags: development, testing, sodium halogen
+---
+
+![simple enough release sketch](./deliver-simple-enough-2.png)
--- src/pages/glossary.js
+++ src/pages/glossary.js
@@ -17,8 +17,9 @@ class GlossaryPage extends React.Component {
           <div>
             <h1>Glossary</h1>
 
-            <GlossaryTerm term="Work Out Load" definition="Sharing your work often for the accountability and feedback. Don't work on an island." />
+            <GlossaryTerm term="3Ws" definition="Prevent your customers from wondering, waiting or worrying, which comes from *Burn Your Portfolio* by Michael Janda." />
             <GlossaryTerm term="Essential Releases" definition="Releasing early with simply-enough so you can get feedback sooner." />
+            <GlossaryTerm term="Work Out Load" definition="Sharing your work often for the accountability and feedback. Don't work on an island. Disseminating knowledge. Giving a pulse of what you are doing and what is going on. The goal is to prevent clients and the team from the 3Ws. See also: 3Ws" />
           </div>
         </div>
       </Layout>
