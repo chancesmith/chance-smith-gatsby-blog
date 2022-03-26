16b0f5beedb7ab5dcb0a5a06a51ed9e1b338d1bd
Chance <chancesmithb@gmail.com>
2022-03-26 15:25:05 -0500

    📝  CONTENT: [index] new intro + coaching ideas

--- src/pages/coaching.js
+++ src/pages/coaching.js
@@ -17,29 +17,41 @@ const WorkshopsPage = (props) => {
         ]}
       />
       <section>
-        <h1>Coaching</h1>
+        <h1>Coaching &amp; Workshops</h1>
         <p>
           On-site or remote, these One-on-One sessions and workshops with Chance
-          are a valuable and effective way to improve your skills as a software
-          developer. Please email me at{" "}
+          are a valuable and effective way to improve your skills as a business
+          owner, software developer, and project manager. Please email me at{" "}
           <a href="mailto:chance@sodiumhalogen.com">chance@sodiumhalogen.com</a>{" "}
           if you have questions at all.
         </p>
 
-        <h2>Pair-Coding Day (6+ hours) - $350</h2>
+        {/* <h2>Strategy Consult (45 mins) - $350</h2>
+        <p>
+          We can discuss actionable plans for your upcoming or ongoing project.
+          This is a One-on-One video chat over Google Meet and you can ask me
+          anything about whatever challenges you're facing. I’ll give you
+          no-nonsense advice that you can put into action immediately,
+          guaranteed.
+        </p>
+        <p>
+          <a href="https://zcal.co/i/WSv5j6E8">Book Now</a>
+        </p> */}
+
+        {/* <h2>Pair-Coding Day (6+ hours) - $350</h2>
         <p>
           A full day of pairing on real projects. You get to see real
           development processes on large projects.
-          {/* <span style={{ backgroundColor: "yellow" }}>
+        </p> */}
+        {/* <span style={{ backgroundColor: "yellow" }}>
             NOTE: Pair-Coding is SOLD OUT through the end of September. Please
             check back in October 2021.
           </span> */}
-        </p>
 
-        <p>
+        {/* <p>
           <a href="https://zcal.co/i/WSv5j6E8">Reserve your spot</a> (limited
           spots)
-        </p>
+        </p> */}
 
         {/* <h2>Workshops</h2> */}
         {/* <p>Reserve your spot for upcoming workshop.</p> */}
--- src/pages/index.js
+++ src/pages/index.js
@@ -23,19 +23,40 @@ class Homepage extends React.Component {
             <h1
               style={{
                 marginTop: "0",
-                fontSize: "3.60675rem",
+                fontSize: "2.5rem",
+                marginBottom: "0.5rem",
               }}
             >
-              Software engineer,
-              <br />
-              <span style={{ fontSize: ".7em", color: "#777" }}>
-                teacher, and efficiency expert.
-                {/* <br />
-                off your team. */}
-              </span>
+              Less hectic, and more impactful, on-time software.
             </h1>
+            <p style={{ fontSize: "1.3rem", color: "#777" }}>
+              Join workshops and 1on1 coaching to build valuable software and
+              software building teams. <br />
+              <Link style={{ boxShadow: `none` }} to={`/coaching/`}>
+                Join a workshop 👩‍💻👨‍💻
+              </Link>
+            </p>
+            {/* <script
+              dangerouslySetInnerHTML={{
+                __html: `<script async data-uid="65c4f812b4" src="https://skilled-knitter-7266.ck.page/65c4f812b4/index.js" />`,
+              }}
+            /> */}
 
-            {[posts[0]].map(({ node }) => {
+            {/* <div>
+              <h2>First time here?</h2>
+              <p>If it's your first time here, the best way to get setteled is to subscribe to my Feature Box Bootcamp. It's a free 3 day email course to teach you how to deliver valuable software faster.</p>
+              <button>Feature Boxing Bootcamp</button>
+            </div> */}
+            <h2
+              style={{
+                marginTop: "0",
+                marginBottom: "0.5rem",
+                fontSize: "1.2rem",
+              }}
+            >
+              Latest Posts
+            </h2>
+            {[...posts.slice(0, 3)].map(({ node }) => {
               const title = node.frontmatter.title || node.fields.slug;
               return (
                 <div
@@ -44,7 +65,12 @@ class Homepage extends React.Component {
                     marginBottom: rhythm(1),
                   }}
                 >
-                  <h2 className="gradient-text">
+                  <h2
+                    className="gradient-text"
+                    style={{
+                      margin: 0,
+                    }}
+                  >
                     <Link
                       style={{ boxShadow: `none`, textDecoration: "none" }}
                       to={node.fields.slug}
@@ -53,7 +79,7 @@ class Homepage extends React.Component {
                       {title}
                     </Link>
                   </h2>
-                  <p
+                  {/* <p
                     style={{
                       ...scale(-1 / 5),
                       display: `block`,
@@ -61,25 +87,28 @@ class Homepage extends React.Component {
                       marginTop: rhythm(-1),
                     }}
                   >
-                    {/* {post.frontmatter.date} */}
                     By{" "}
                     <a href="/about" style={{ textDecoration: "none" }}>
                       Chance Smith
                     </a>
                   </p>
-                  <div dangerouslySetInnerHTML={{ __html: node.html }} />
-
-                  {/* <small>{node.frontmatter.date}</small> */}
-                  {/* <p dangerouslySetInnerHTML={{ __html: node.excerpt }} /> */}
+                  <div dangerouslySetInnerHTML={{ __html: node.html }} /> */}
+                  <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+                  <Link
+                    style={{ boxShadow: `none`, textDecoration: "none" }}
+                    to={node.fields.slug}
+                    title={title}
+                  >
+                    Continue Reading &gt;&gt;
+                  </Link>
                 </div>
               );
             })}
-
-            <div style={{ textAlign: "center" }}>
+            {/* <div style={{ textAlign: "center" }}>
               <Link to={"/archive"} style={{ textDecoration: "none" }}>
                 See All Posts &gt;&gt;
               </Link>
-            </div>
+            </div> */}
           </div>
         </div>
       </Layout>
