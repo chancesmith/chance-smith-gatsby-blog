c5fcea91e3cc82a3c8f68f5fcc28fa5d1f2a3aaa
Chance Smith <chance.smith@lifeway.com>
2023-05-22 11:59:33 -0500

    📝 CONTENT: add uses page

--- src/pages/uses.js
+++ src/pages/uses.js
@@ -9,15 +9,29 @@ class UsesPage extends React.Component {
   render() {
     return (
       <Layout location={this.props.location} title={"Chance uses"}>
-        <SEO
-          title="Chance uses"
-          keywords={[`Chance uses`]}
-        />
+        <SEO title="Chance uses" keywords={[`Chance uses`]} />
         <div>
           <div>
             <h1>Uses</h1>
 
-            <p>coming soon...</p>
+            <h2>Ethos / Mantras</h2>
+            <p>Phrases you'll hear me say often.</p>
+            <ul>
+              <li>
+                Listen to the whispers before you have endure more screams.
+              </li>
+              <li>
+                Make it easy to do the right thing and hard to do the wrong
+                thing.
+              </li>
+              <li>
+                Would you rather clean a room with 100 items or zero? Get closer
+                to zero.
+              </li>
+              <li>
+                I vital skill to have is the ability to duplicate yourself.
+              </li>
+            </ul>
           </div>
         </div>
       </Layout>
